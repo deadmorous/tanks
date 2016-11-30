df4d8cb0e8bb738e9ef87e1ee70797881bb38827
Fury <bmx_tricks@mail.ru>
2016-12-01 00:27:02 +0300

    bad request on /enter

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+router
+    .get('/', function(req, res, next) {
+        res.render('');
+    })
+    .post('/', function(req, res, next) {
+        req.session.name = req.body.name
+        res.redirect('/game/enter')
+    })
+    .use(function(req, res, next) {
+        if (!req.session.name)
+            return res.redirect('/')
+        next()
+    })
+
 // Describes player position
 function Position()
 {
